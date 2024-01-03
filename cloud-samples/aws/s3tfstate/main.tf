082e9275e70f0e2901375160451f1c6cc88c3b92
Przemyslaw Swierczek <swierq@gmail.com>
2024-01-03 12:05:19 +0100

    S3 Bucket for Tfstate.

---
+++ cloud-samples/aws/s3tfstate/main.tf
@@ -0,0 +1,74 @@
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "tfstate-prz"
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+
+resource "aws_s3_bucket_versioning" "terraform_state" {
+  bucket = aws_s3_bucket.terraform_state.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+
+resource "aws_dynamodb_table" "terraform_state_lock" {
+  name           = "tfstate-prz"
+  read_capacity  = 1
+  write_capacity = 1
+  hash_key       = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
+
+# resource "aws_s3_bucket_policy" "terraform_state" {
+#   bucket = "${aws_s3_bucket.terraform_state.id}"
+#   policy =<<EOF
+# {
+#   "Version": "2012-10-17",
+#   "Id": "RequireEncryption",
+#    "Statement": [
+#     {
+#       "Sid": "RequireEncryptedTransport",
+#       "Effect": "Deny",
+#       "Action": ["s3:*"],
+#       "Resource": ["arn:aws:s3:::${aws_s3_bucket.terraform_state.bucket}/*"],
+#       "Condition": {
+#         "Bool": {
+#           "aws:SecureTransport": "false"
+#         }
+#       },
+#       "Principal": "*"
+#     },
+#     {
+#       "Sid": "RequireEncryptedStorage",
+#       "Effect": "Deny",
+#       "Action": ["s3:PutObject"],
+#       "Resource": ["arn:aws:s3:::${aws_s3_bucket.terraform_state.bucket}/*"],
+#       "Condition": {
+#         "StringNotEquals": {
+#           "s3:x-amz-server-side-encryption": "AES256"
+#         }
+#       },
+#       "Principal": "*"
+#     }
+#   ]
+# }
+# EOF
+# }
